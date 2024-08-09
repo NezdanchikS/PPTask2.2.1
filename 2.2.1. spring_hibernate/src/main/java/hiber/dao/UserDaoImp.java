d7fe17a0cfaf15bcb26f0c5c528f4a1006ac54a4
NeazdanchikS <NezdanchikS@yandex.ru>
2024-08-09 22:48:09 +0300

    Initial commit

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -1,5 +1,6 @@
 package hiber.dao;
 
+import hiber.model.Car;
 import hiber.model.User;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,19 +12,29 @@ import java.util.List;
 @Repository
 public class UserDaoImp implements UserDao {
 
-   @Autowired
-   private SessionFactory sessionFactory;
+    @Autowired
+    private SessionFactory sessionFactory;
 
-   @Override
-   public void add(User user) {
-      sessionFactory.getCurrentSession().save(user);
-   }
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
 
-   @Override
-   @SuppressWarnings("unchecked")
-   public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
-      return query.getResultList();
-   }
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
+
+    @Override
+    public List<User> getUserByCar(String model, int series) {
+        String HQL = "select u from User u LEFT JOIN u.userCar c WHERE c.model =: model AND c.series=:series";
+        TypedQuery<User> query = sessionFactory.getCurrentSession()
+                .createQuery(HQL, User.class)
+                .setParameter("model", model)
+                .setParameter("series", series);
+        return query.getResultList();
+    }
 
 }
--- 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
@@ -8,6 +8,7 @@ public class User {
 
    @Id
    @GeneratedValue(strategy = GenerationType.IDENTITY)
+   @Column(name = "id")
    private Long id;
 
    @Column(name = "name")
@@ -18,6 +19,10 @@ public class User {
 
    @Column(name = "email")
    private String email;
+
+   @OneToOne (cascade = CascadeType.ALL)
+   @JoinColumn(name = "car_id")
+   private Car userCar;
 
    public User() {}
    
@@ -57,5 +62,23 @@ public class User {
 
    public void setEmail(String email) {
       this.email = email;
+   }
+
+   public Car getUserCar() {
+      return userCar;
+   }
+
+   public void setUserCar(Car userCar) {
+      this.userCar = userCar;
+   }
+
+   @Override
+   public String toString() {
+      return "UserInfo" +
+              "\nid=" + id +
+              "\nfirstName='" + firstName + '\'' +
+              "\nlastName='" + lastName + '\'' +
+              "\nemail='" + email + '\'' +
+              "\nuserCar=" + userCar;
    }
 }
